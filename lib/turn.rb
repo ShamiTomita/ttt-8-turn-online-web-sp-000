0ec794fe64969634eb6b41ca72fa6312e0e60be8
Shami Tomita <shamitomita@gmail.com>
2020-12-12 18:56:39 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+def turn(board)
+end 
+
+def display_board(board)
+  puts "#{board[0]},"
+end 
+
+def input_to_index(input)
+  input.to_i-1
+end 
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end 
+
+def valid_move?(board, index)
+  if index.between(0,8) && position_taken?(board, index) == false 
+    return true 
+  else 
+    return false
+  end 
+end 
